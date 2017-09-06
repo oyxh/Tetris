765b241b40e00bddfde690df5fe8e91426df95ef
oyxh <13975812120@139.com>
2017-09-06 19:38:46 +0800

    from home

---
+++ Tetris/Assets/Scripts/GUIManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GUIManager : MonoBehaviour {
+
+	// Use this for initialization
+    float start_time;
+    Text timer;
+	void Start () {
+        start_time = Time.time;
+        timer = GameObject.Find("Canvas/Timer").GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float addTime = Time.time - start_time;
+        int minutes = (int)addTime / 60;
+        int seconds = (int)addTime % 60;
+        string strTime = string.Format("{0:00}:{1:00}",minutes,seconds);
+        timer.text = strTime;
+	}
+}
---
+++ Tetris/Assets/Scripts/Spawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject[] groups;
+	// Use this for initialization
+	void Start () {
+        spawnNext();
+	}
+	
+	// 生成一个随机方块
+    void spawnNext()
+    {
+        int i = Random.Range(0, groups.Length);
+        GameObject ins = Instantiate(groups[i], transform.position, Quaternion.identity);
+    }
+}
